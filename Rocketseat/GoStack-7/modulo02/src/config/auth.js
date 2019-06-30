e73d2bf71b232f031ab13c46ad2ca94e9e17bb0e
macmessa <mac.messa@gmail.com>
2019-06-30 09:44:54 -0300

    feat: create, update and authenticate users
    Finish modulo02 features.
    
    Adds:
    - Model loader;
    - User creation and update;
    - Password hash creation with bcrypt;
    - Implement JWT to login;
    - Middleware to validate routes that must be authorized;
    - Validating user imput through Yup.

---
+++ Rocketseat/GoStack-7/modulo02/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3cea16991b79d62f99cfef10161e8b8d',
+  expiresIn: '7d',
+};
--- Rocketseat/GoStack-7/modulo02/src/routes.js
+++ Rocketseat/GoStack-7/modulo02/src/routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Marco Aurélio Messa',
-    email: 'mac.messa@gmail.com',
-    password_hash: '123456789',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
