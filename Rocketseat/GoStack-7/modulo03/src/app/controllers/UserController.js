3c8f8eec038322c059f3a59f86748e8c2027f66f
macmessa <mac.messa@gmail.com>
2019-07-04 23:48:23 -0300

    fix: check if email was sent from the request

--- Rocketseat/GoStack-7/modulo03/src/app/controllers/UserController.js
+++ Rocketseat/GoStack-7/modulo03/src/app/controllers/UserController.js
@@ -50,7 +50,7 @@ class UserController {
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
@@ -62,12 +62,14 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    const { id, name, email: userEmail, provider } = await user.update(
+      req.body
+    );
 
     return res.json({
       id,
       name,
-      email,
+      email: userEmail,
       provider,
     });
   }
