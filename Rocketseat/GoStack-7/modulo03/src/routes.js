65931b404c96cf27cc73e321ea25faacde2d13bf
macmessa <mac.messa@gmail.com>
2019-07-04 22:44:48 -0300

    feat: list provider schedule

--- Rocketseat/GoStack-7/modulo03/src/routes.js
+++ Rocketseat/GoStack-7/modulo03/src/routes.js
@@ -2,13 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
-import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,6 +29,8 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 // Create appointment
 routes.post('/appointments', AppointmentController.store);
+// Create appointment
+routes.get('/schedule', ScheduleController.index);
 // Upload picture
 routes.post('/files', upload.single('file'), FileController.store);
 
