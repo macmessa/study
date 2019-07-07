b32c7df6c01c3c2e37212c874c70768c34d3d306
macmessa <mac.messa@gmail.com>
2019-07-07 11:06:16 -0300

    feat: list all provider's notifications

--- Rocketseat/GoStack-7/modulo03/src/routes.js
+++ Rocketseat/GoStack-7/modulo03/src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,8 +30,10 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 // Create appointment
 routes.post('/appointments', AppointmentController.store);
-// Create appointment
+// List schedules
 routes.get('/schedule', ScheduleController.index);
+// List notifications
+routes.get('/notifications', NotificationController.index);
 // Upload picture
 routes.post('/files', upload.single('file'), FileController.store);
 
