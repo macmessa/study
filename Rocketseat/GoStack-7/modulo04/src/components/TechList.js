6f7ff3d3720d88c9bbcdb89de77f36197aa04c6b
macmessa <mac.messa@gmail.com>
2019-07-26 23:22:50 -0300

    feat: first component

---
+++ Rocketseat/GoStack-7/modulo04/src/components/TechList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class TechList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>NodeJS</li>
+        <li>React</li>
+        <li>React Native</li>
+      </ul>
+    );
+  }
+}
+
+export default TechList;
