e45cfedad02b6b87eab471f0c018146045f4e76b
macmessa <mac.messa@gmail.com>
2019-08-03 23:17:18 -0300

    feat: app basic layout with styled components

--- Rocketseat/GoStack-7/modulo05/src/App.js
+++ Rocketseat/GoStack-7/modulo05/src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
