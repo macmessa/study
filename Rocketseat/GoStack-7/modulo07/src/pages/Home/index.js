7255453d6520c3eb502c6738b753995664790e58
macmessa <mac.messa@gmail.com>
2019-08-25 15:43:27 -0300

    feat(Home): product list

--- Rocketseat/GoStack-7/modulo07/src/pages/Home/index.js
+++ Rocketseat/GoStack-7/modulo07/src/pages/Home/index.js
@@ -1,7 +1,112 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/26/D12-1407-026/D12-1407-026_detalhe1.jpg?resize=326:*"
+          alt="Tênis Nike Md Runner 2 Masculino"
+        />
+        <strong>Tênis Nike Md Runner 2 Masculino</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 1
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/26/D12-1407-026/D12-1407-026_detalhe1.jpg?resize=326:*"
+          alt="Tênis Nike Md Runner 2 Masculino"
+        />
+        <strong>Tênis Nike Md Runner 2 Masculino</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 1
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/26/D12-1407-026/D12-1407-026_detalhe1.jpg?resize=326:*"
+          alt="Tênis Nike Md Runner 2 Masculino"
+        />
+        <strong>Tênis Nike Md Runner 2 Masculino</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 1
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/26/D12-1407-026/D12-1407-026_detalhe1.jpg?resize=326:*"
+          alt="Tênis Nike Md Runner 2 Masculino"
+        />
+        <strong>Tênis Nike Md Runner 2 Masculino</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 1
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/26/D12-1407-026/D12-1407-026_detalhe1.jpg?resize=326:*"
+          alt="Tênis Nike Md Runner 2 Masculino"
+        />
+        <strong>Tênis Nike Md Runner 2 Masculino</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 1
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/26/D12-1407-026/D12-1407-026_detalhe1.jpg?resize=326:*"
+          alt="Tênis Nike Md Runner 2 Masculino"
+        />
+        <strong>Tênis Nike Md Runner 2 Masculino</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 1
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
