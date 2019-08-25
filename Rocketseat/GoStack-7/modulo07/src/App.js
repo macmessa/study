3d7e78d6c113cab5c1ec037a26aeb33ef4e630c0
macmessa <mac.messa@gmail.com>
2019-08-25 15:08:43 -0300

    feat: global style and header

--- Rocketseat/GoStack-7/modulo07/src/App.js
+++ Rocketseat/GoStack-7/modulo07/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Hello Redux</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
